7c0d2f856d9678fabc58ae5805ed6c62e8b2f170
Shivam_Shivanshu <shivamshivanshu15@gmail.com>
2021-05-23 18:43:34 +0530

    Update template.cpp

--- configuration/template.cpp
+++ configuration/template.cpp
@@ -2,13 +2,14 @@
 /*    The greatest journey made is the journey made inwards, towards yourself.         */
 
 #include <bits/stdc++.h>
+using namespace std;
 /*    It's what you practice in private that you will be rewarded for in public       */
 
 #define gc getchar_unlocked
 #define endl "\n"
 #define fo(i, n) for (int i = 0; i < n; i++)
 #define rep(i, k, n) for (int i = k; i <= n; i++)
-#define yes(i)	std::cout << (i > 0 ? "Yes\n" : "No\n")
+#define yes(i) std::cout << (i > 0 ? "Yes\n" : "No\n")
 #define ll long long int
 #define ull unsigned long long int
 #define deb(x) std::cout << #x << "=" << x << endl
@@ -44,32 +45,70 @@ typedef std::vector<vl> vvl;
 std::mt19937_64 rang(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 int rng(int lim)
 {
-	std::uniform_int_distribution<int> uid(0, lim - 1);
-	return uid(rang);
+    std::uniform_int_distribution<int> uid(0, lim - 1);
+    return uid(rang);
 }
 /* Talent is a pursued interest. Anything that you're willing to practice, you can do */
 
-template<class T> bool chmax(T& a, const T b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> bool chmin(T& a, const T b) { if (b < a) { a = b; return 1; } return 0; }
-template<typename T> void db(T var)	{std::cerr << var << " ";}
-template<typename T, typename... Types> void db(T var1, Types... var2)	{std::cerr << var1 << " "; db(var2...);}
-template<typename T> void print(T var)	{std::cout << var << endl;}
-template<typename T, typename... Types> void print(T var1, Types... var2)	{std::cerr << var1 << " "; print(var2...);}
-template<typename T> void print_container(const T& container) {for (auto element : container)	{std::cout << element << " ";}	std::cout << endl;}
+template <class T>
+bool chmax(T &a, const T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+bool chmin(T &a, const T b)
+{
+    if (b < a)
+    {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <typename T>
+void db(T var) { std::cerr << var << endl; }
+template <typename T, typename... Types>
+void db(T var1, Types... var2)
+{
+    std::cerr << var1 << " ";
+    db(var2...);
+}
+template <typename T>
+void print(T var) { std::cout << var << endl; }
+template <typename T, typename... Types>
+void print(T var1, Types... var2)
+{
+    std::cerr << var1 << " ";
+    print(var2...);
+}
+template <typename T>
+void print_container(const T &container)
+{
+    for (auto element : container)
+    {
+        std::cout << element << " ";
+    }
+    std::cout << endl;
+}
 bool isPrime(ll n)
 {
-	if (n <= 1)
-		return false;
-	if (n <= 3)
-		return true;
-	if (n % 2 == 0 || n % 3 == 0)
-		return false;
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
 
-	for (int i = 5; i * i <= n; i = i + 6)
-		if (n % i == 0 || n % (i + 2) == 0)
-			return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
 
-	return true;
+    return true;
 }
 /*_____________________________________________________________________________________*/
 const int INF = 1e9;
@@ -83,12 +122,13 @@ void solve()
 
 int32_t main()
 {
-	FAST;
-	int test_case = 1;
-	std::cin >> test_case;
-	while (test_case--) {
-		solve();
-	}
-	return 0;
+    FAST;
+    int test_case = 1;
+    std::cin >> test_case;
+    while (test_case--)
+    {
+        solve();
+    }
+    return 0;
 }
 /*_____________________________________________________________________________________*/
