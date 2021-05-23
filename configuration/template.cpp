a69b397b0a52b917d33d05d16c43bcbd4b8f0933
Shivam_Shivanshu <shivamshivanshu15@gmail.com>
2021-05-23 18:44:05 +0530

    Update template.cpp

--- configuration/template.cpp
+++ configuration/template.cpp
@@ -83,7 +83,7 @@ void print(T var) { std::cout << var << endl; }
 template <typename T, typename... Types>
 void print(T var1, Types... var2)
 {
-    std::cerr << var1 << " ";
+    std::cout << var1 << " ";
     print(var2...);
 }
 template <typename T>
